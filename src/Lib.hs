571d0d48872f116bcae9d249e0db30e9f8238ba7
Román Ignacio Muñoz <rommunoz@frba.utn.edu.ar>
2023-06-05 19:57:47 -0300

    Punto 5 respuestas al final

--- src/Lib.hs
+++ src/Lib.hs
@@ -16,10 +16,10 @@ data Color = Negro | Rojo | Azul | Blanco deriving (Eq)
 
 estaCercaDe :: Auto -> Auto -> Bool
 estaCercaDe unAuto otroAuto = 
-    sonDistintos unAuto otroAuto && ((< 10) . abs . substract (distancia unAuto ))(distancia otroAuto)
+    (not . sonIguales unAuto) otroAuto && ((< 10) . abs . substract (distancia unAuto ))(distancia otroAuto)
 
-sonDistintos :: Auto -> Auto -> Bool
-sonDistintos unAuto otroAuto = color unAuto /= color otroAuto
+sonIguales :: Auto -> Auto -> Bool
+sonIguales unAuto otroAuto = color unAuto == color otroAuto
 
 vaTranquilo :: Auto -> Carrera -> Bool
 vaTranquilo unAuto unaCarrera = all (not . estaCercaDe $ unAuto) unaCarrera && lesVaGanandoATodos unAuto unaCarrera
@@ -57,16 +57,19 @@ restarVelocidad unaVelocidad otraVelocidad
 
 type PowerUp = (Carrera -> Carrera)
 
-terremoto :: PowerUp
+terremoto :: Auto -> PowerUp
 terremoto unAuto = afectarALosQueCumplen (estaCercaDe unAuto) (bajarLaVelocidad 50)
 
-miguelitos :: Int -> PowerUp
+miguelitos :: Int -> Auto -> PowerUp
 miguelitos unaVelocidad unAuto = afectarALosQueCumplen (leVaGanando unAuto) (bajarLaVelocidad unaVelocidad)
 
-jetPack :: Int -> PowerUp
+jetPack :: Int -> Auto -> PowerUp
 jetPack unTiempo unAuto = 
-    afectarALosQueCumplen ((==) color unAuto) (mapVelocidad (const velocidad unAuto) . queCorra unTiempo . mapVelocidad (*2))
+    afectarALosQueCumplen (sonIguales unAuto) (mapVelocidad (const velocidad unAuto) . queCorra unTiempo . mapVelocidad (*2))
 
+{- misilTeledirigido :: Auto -> Color -> PowerUp
+misilTeledirigido unAuto colorDeUnaVictima = 
+    afectarALosQueCumplen ((==) colorDeUnaVictima . color) (accionMisil) -}
 -------------
 -- Punto 4 --
 -------------
@@ -81,19 +84,19 @@ simularCarrera unaCarrera unosEventos =
 correnTodos :: Int -> Evento
 correnTodos unTiempo = map (queCorra unTiempo)
 
-usaPowerUp :: Auto -> PowerUp -> Evento 
-usaPowerUp unAuto unPowerUp = afectarALosQueCumplen (== (color unAuto)) (unPowerUp) 
+usaPowerUp :: Color -> PowerUp -> Evento 
+usaPowerUp unColor unPowerUp = afectarALosQueCumplen ((==) unColor . color) (unPowerUp) 
 
 carrera :: Carrera
 carrera = [Auto Blanco 120 0, Auto Azul 120 0, Auto Negro 120 0, Auto Rojo 120 0]
 
 serieDeEventos :: [Evento]
 serieDeEventos = [correnTodos 30, 
-        usaPowerUp Azul jetPack 3, 
+        usaPowerUp Azul (jetPack 3), 
         usaPowerUp Blanco terremoto, 
         correnTodos 40,
-        usaPowerUp Blanco miguelitos 20,
-        usaPowerUp Negro jetPack 6,
+        usaPowerUp Blanco (miguelitos 20),
+        usaPowerUp Negro (jetPack 6),
         correnTodos 10]
 
 carreraPedida :: [Puesto]
@@ -103,4 +106,11 @@ carreraPedida = simularCarrera carrera serieDeEventos
 -- Punto 5 --
 -------------
 
--- 
+-- a) fue dificil pero se me ocurrio algo despues de 20 minutos
+{- 
+misilTeledirigido :: Auto -> Color -> PowerUp
+misilTeledirigido unAuto colorDeUnaVictima = 
+    afectarALosQueCumplen ((==) colorDeUnaVictima . color) (accionMisil)
+ -} -- el primer parametro me queda sin usar pero serviria para saber quien le manda el misil
+
+-- b) la 1b depende, si hay un auto que no cumpla. Y la 1c no, porque tiene que recorrer toda la lista
